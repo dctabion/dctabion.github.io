29596e0b3dc3bbb4beb77eb3bbf03f76b3825b52
Daniel Tabion <dctabion@gmail.com>
2015-11-10 19:42:38 -0600

    added event listeners & stuff

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,7 +1,7 @@
 $('document').ready(function(){
   console.log('window loaded');
 
-  ///////////// Preload & Prep /////////////
+  ///////////// Preload Resources: images & sounds /////////////
 
   // Images
   var imgRoach = new Image();
@@ -19,11 +19,18 @@ $('document').ready(function(){
   // some gobals
   var canvasWidth = 600; // from CSS file ...make this adjustable in future?
   var canvasHeight = 500; // from CSS file ...make this adjustable in future?
+
+  var roaches = [];
   var numRoaches = 5;
   var maxRoachSpeed = 5;
-  var roaches = [];
+  var minRoachSpeed = 1;
+  var startRadius = 25;
 
-  // Constructors
+  var timeoutId;
+
+  //////////////// Constructors ////////////////
+
+  // Make new roach
   function roach(xPos, yPos, xVel, yVel) {
     this.xPos = xPos;
     this.yPos = yPos;
@@ -43,36 +50,90 @@ $('document').ready(function(){
     }
     this.render = function() {
       ctx.drawImage(imgRoach, this.xPos, this.yPos);
-
+    };
     this.changeVelocity = function() {
       // Future feature in new version
       // make roaches respond to mouse proximity
       }
+  }
+
+  // Make new player
+  function player() {
+    this.kills = 0;
+    this.name = "Dood";
+    this.killRoaches = function() {
+      console.log('kill roaches')
+
 
     };
   }
 
-  // Initialize objects & call construtors
+  ////////////////  Helper Functions /////////////////
+
+  // move and draw all the roaches
+  function renderGame() {
+    console.log('rendering');
+    for (var i=0; i<roaches.length; i++) {
+      roaches[i].moveIt();
+    }
+    for (var i=0; i<roaches.length; i++) {
+      roaches[i].clear();
+    }
+    for (var i=0; i<roaches.length; i++) {
+      roaches[i].render();
+    }
+  }
+
+  /////// MAIN CONTROL CODE ///////////////////
+  /////////////////////////////////////////////
+
+  ///////// Initialize objects & call construtors //////////
+
+  // Create the roaches with initial random positions & velcities
   for (var i=0; i<numRoaches; i++) {
     console.log('creating Roach ' + i);
-    var x = parseInt((Math.random() * 50 ) + (canvasWidth / 2 - 50));
-    var y = parseInt((Math.random() * 50 ) + (canvasHeight / 2 - 50));
-    var xVel = parseInt(Math.random() * maxRoachSpeed);
-    var yVel = parseInt(Math.random() * maxRoachSpeed);
+    // choose random position within 50px of center
+    var x = Math.floor(
+      Math.random() * (  ((canvasWidth/2 + startRadius)+1) - (canvasWidth/2 - startRadius)  )
+      + (canvasWidth/2 - startRadius)
+    );
+    var y = Math.floor(
+      Math.random() * (  ((canvasHeight/2 + startRadius)+1) - (canvasHeight/2 - startRadius)  )
+      + (canvasHeight/2 - startRadius)
+    );
+
+    // choose random velocity from +/-maxRoachSpeed
+    var xVel = Math.floor(
+      Math.random() * ((maxRoachSpeed+1) - minRoachSpeed) + minRoachSpeed
+      - maxRoachSpeed/2
+    );
+    var yVel = Math.floor(
+      Math.random() * ((maxRoachSpeed+1) - minRoachSpeed) + minRoachSpeed
+      - maxRoachSpeed/2
+    );
     roaches[i] = new roach(x, y, xVel, yVel);
   }
 
-  // Helper Functions
-  function renderGame() {
-    for (var i=0; i<roaches.length) {
-      roaches[i].moveIt();
-      roaches[i].clear();
-      roaches[i].render();
+  // Create player
+  var player1 = new player();
+
+  // listen for player attempting to kill roaches with mouse click
+  canvas.addEventListener('click', function(event) {
+    console.log('click');
+  });
+  // ctx.addEventListener('click', player1.killRoaches(event) );
+
+  // listen for ESC to stop the program
+  $( "html" ).keydown(function( event ) {
+    // ESC - stop the roach!
+    if (event.keyCode == 27) {
+      clearInterval(timeoutId);
     }
-    // alert();
-  }
+    console.log('event.keyCode: ' + event.keyCode);
+  });
 
   // set game rendering in motion
-  timeoutID = window.setInterval(renderGame, 50);
+  timeoutId = window.setInterval(renderGame, 50);
+//  renderGame();
 
 }); // close $('document').ready()
